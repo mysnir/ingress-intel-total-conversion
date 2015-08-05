a52ef893513d7bfde0596bdf2142f43c34ac4d9a
Yoram Snir <mysnir@users.noreply.github.com>
2015-08-05 16:48:18 -0400

    Create portals-list.meta.js

---
+++ build/plugins/portals-list.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @id             iitc-plugin-portals-list@teo96
+// @name           IITC plugin: show list of portals
+// @category       Info
+// @version        0.2.1.20150805.203419
+// @namespace      https://github.com/mysnir/ingress-intel-total-conversion
+// @updateURL      https://github.com/mysnir/ingress-intel-total-conversion/raw/master/plugins/portals-list.meta.js
+// @downloadURL    https://github.com/mysnir/ingress-intel-total-conversion/raw/master/plugins/portals-list.user.js
+// @description    [local-2015-08-05-203419] Display a sortable list of all visible portals with full details about the team, resonators, links, etc.
+// @include        https://www.ingress.com/intel*
+// @include        http://www.ingress.com/intel*
+// @match          https://www.ingress.com/intel*
+// @match          http://www.ingress.com/intel*
+// @include        https://www.ingress.com/mission/*
+// @include        http://www.ingress.com/mission/*
+// @match          https://www.ingress.com/mission/*
+// @match          http://www.ingress.com/mission/*
+// @grant          none
+// ==/UserScript==
