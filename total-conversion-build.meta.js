8d62914ba15e7ef066946371a5f77a21cc75c460
Yoram Snir <mysnir@users.noreply.github.com>
2015-08-05 16:40:12 -0400

    Create total-conversion-build.meta.js

---
+++ total-conversion-build.meta.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @id             ingress-intel-total-conversion@jonatkins
+// @name           IITC: Ingress intel map total conversion
+// @version        0.24.1.20150805.203419
+// @namespace      https://github.com/mysnir/ingress-intel-total-conversion
+// @updateURL      https://github.com/mysnir/ingress-intel-total-conversion/raw/master/total-conversion-build.meta.js
+// @downloadURL    https://github.com/mysnir/ingress-intel-total-conversion/raw/master/total-conversion-build.user.js
+// @description    [local-2015-08-05-203419] Total conversion for the ingress intel map.
+// @include        https://www.ingress.com/intel*
+// @include        http://www.ingress.com/intel*
+// @match          https://www.ingress.com/intel*
+// @match          http://www.ingress.com/intel*
+// @include        https://www.ingress.com/mission/*
+// @include        http://www.ingress.com/mission/*
+// @match          https://www.ingress.com/mission/*
+// @match          http://www.ingress.com/mission/*
+// @grant          none
+// ==/UserScript==
