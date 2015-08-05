0319443f516f3f7161988f7a24707e697b5ded4e
Yoram Snir <mysnir@users.noreply.github.com>
2015-08-05 16:49:12 -0400

    Update portals-list.user.js

--- build/plugins/portals-list.user.js
+++ build/plugins/portals-list.user.js
@@ -3,7 +3,7 @@
 // @name           IITC plugin: show list of portals
 // @category       Info
 // @version        0.2.1.20150805.203419
-// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @namespace      https://github.com/mysnir/ingress-intel-total-conversion
 // @updateURL      https://github.com/mysnir/ingress-intel-total-conversion/raw/master/plugins/portals-list.meta.js
 // @downloadURL    https://github.com/mysnir/ingress-intel-total-conversion/raw/master/plugins/portals-list.user.js
 // @description    [local-2015-08-05-203419] Display a sortable list of all visible portals with full details about the team, resonators, links, etc.
